4c05e5c0f3e08ef1b929e4fe5eff2b1faa8e4add
kaptensifu <150881290+kaptensifu@users.noreply.github.com>
2023-11-26 16:40:18 +0700

    Add files via upload

---
+++ page/admin/input.php
@@ -0,0 +1,17 @@
+<?php
+include "../../core/koneksi.php";
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$role = $_POST['role'];
+$query = mysqli_query(
+    $konek,
+    "INSERT INTO users
+VALUES('','$username','$email','$password','$role','')"
+) or die(mysqli_error($konek));
+if ($query) {
+    header("location:users.php");
+} else {
+    echo "Proses Input gagal";
+}
+?>
